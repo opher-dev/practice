5b8153441b0a68f1186256925019288f9dc4186f
Baatile <batilelanga@hotmail.com>
2023-04-05 00:07:00 +0200

    XML Implimentation

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 import express from "express";
+import { lessons } from "./data";
 import cors from "cors";
 const app = express();
 const port = 4377;
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.get("/", (req, resp) => {
-  resp.status(200).send({ message: "Voilor!", lessons: [4, 5, 5] });
+  resp.status(200).send({ message: "Voilor!", lessons});
 });
 app.listen(port, () => {
   console.log("Server running on port http://localhost:4377");
