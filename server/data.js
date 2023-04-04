f5ef82940cf55b3651d9450ef46f205d001422d5
Baatile <batilelanga@hotmail.com>
2023-04-04 23:46:10 +0200

    my jason data

---
+++ server/data.js
@@ -0,0 +1,23 @@
+export const lessons =[{
+     
+          topic:'Module 1',
+          id:1,
+          description:'',
+          outcomes:['','','']
+        
+}, {
+    topic:'Module 1',
+    id:1,
+    description:'',
+    outcomes:['','','']
+  }, {
+    topic:'Module 1',
+    id:1,
+    description:'',
+    outcomes:['','','']
+  }, {
+    topic:'Module 1',
+    id:1,
+    description:'',
+    outcomes:['','','']
+  },]
